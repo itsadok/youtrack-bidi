3abf3966bcd2631183fd4bcc1b816e43ca6d093a
Israel Tsadok <itsadok@gmail.com>
2013-03-06 06:17:33 +0200

    Better reaction for dynamic changes

--- js/content_script.js
+++ js/content_script.js
@@ -1,23 +1,30 @@
 $(function() {
-	$(".wiki.text").each(function() {
-		var str = $(this).text();
-		var firstChar = str.charCodeAt(str.search(/[^A-Z\s\d.,-]/));
-		if(firstChar >= 1424 && firstChar <= 1983) {
-			$(this).css({direction: "rtl", "text-align": "right", "overflow-x": "auto"});
-		}
-	});
+    function textAreaKeyDown() {
+        var str = $(this).val();
+        var firstChar = str.charCodeAt(str.search(/[^A-Z\s\d.,-]/));
+        if(firstChar >= 1424 && firstChar <= 1983) {
+            $(this).css({direction: "rtl", "text-align": "right"});
+            $(this).parent().parent().parent()
+                .find(".comment-preview .wiki.text").css({direction: "rtl", "text-align": "right"});
+        } else {
+            $(this).css({direction: "ltr", "text-align": "left"});
+            $(this).parent().parent().parent()
+                .find(".comment-preview .wiki.text").css({direction: "ltr", "text-align": "left"});
+        }
+    }
 
-	$("textarea").keydown(function() {
-		var str = $(this).val();
-		var firstChar = str.charCodeAt(str.search(/[^A-Z\s\d.,-]/));
-		if(firstChar >= 1424 && firstChar <= 1983) {
-			$(this).css({direction: "rtl", "text-align": "right"});
-            $(".comment-preview .wiki.text").css({direction: "rtl", "text-align": "right"});
-		} else {
-			$(this).css({direction: "ltr", "text-align": "left"});
-            $(".comment-preview .wiki.text").css({direction: "ltr", "text-align": "left"});
-		}
-	});
+    function alignToText() {
+        $(".wiki.text,textarea").each(function() {
+            var str = $(this).val() || $(this).text();
+            var firstChar = str.charCodeAt(str.search(/[^A-Z\s\d.,-]/));
+            if(firstChar >= 1424 && firstChar <= 1983) {
+                $(this).css({direction: "rtl", "text-align": "right", "overflow-x": "auto"});
+            }
+        });
+
+        $("textarea").unbind("keydown", textAreaKeyDown).keydown(textAreaKeyDown);
+    }
+    alignToText();
 
     var observer = new WebKitMutationObserver(function(mutations, observer) {
         var relevant = false;
@@ -25,15 +32,9 @@ $(function() {
             if(this.addedNodes.length > 0) relevant = true;
         });
         if(relevant) {
-            $(".wiki.text").each(function() {
-                var str = $(this).text();
-                var firstChar = str.charCodeAt(str.search(/[^A-Z\s\d.,-]/));
-                if(firstChar >= 1424 && firstChar <= 1983) {
-                    $(this).css({direction: "rtl", "text-align": "right", "overflow-x": "auto"});
-                }
-            });
+            alignToText();
         }
     });
-    observer.observe($(".issue-comments")[0], { subtree: true });
+    observer.observe($(".issue-comments")[0], { attributes: true, childList: true, characterData: true, subtree: true });
 });
 
